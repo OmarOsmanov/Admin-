4c3d7caf110e4d012464793e7dace4d7b928475e
OmarOsmanov <tu201906019@code.edu.az>
2021-12-29 21:11:22 +0400

    first commit

---
+++ jkhkhkuhuh/Models/CustomUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jkhkhkuhuh.Models
+{
+    public class CustomUser : IdentityUser
+    {
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Surname { get; set; }
+        [MaxLength(500)]
+        [NotMapped, Required]
+        public string RoleId { get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
